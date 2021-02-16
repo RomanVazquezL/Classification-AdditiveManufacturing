8bca27708a346e319f5084eac52cca7214c1a4a9
RomanVazquezL <63014545+RomanVazquezL@users.noreply.github.com>
2021-02-16 11:43:01 +0100

    Update HowtoReadandCutvideo.m

--- HowtoReadandCutvideo.m
+++ HowtoReadandCutvideo.m
@@ -1,7 +1,7 @@
 clear all; close all; clc;
 %% Display video using depVideoPlayer
 %Create deployable video player and video writer
-VR = vision.VideoFileReader('Until2ndSpark24fps.avi');
+VR = vision.VideoFileReader('fullvideo24fps.avi');
 depVideoPlayer = vision.DeployableVideoPlayer;
 % while loop to play video frame by  frame
 count = ~isDone(VR); %check if v is not done, returns logical where 0 if not done
@@ -11,19 +11,19 @@ count = ~isDone(VR); %check if v is not done, returns logical where 0 if not don
     count = ~isDone(VR) && isOpen(depVideoPlayer); %update count to close when 0 
   end  
 %% Display video using Video Reader and hasFrames loop
-v = VideoReader('fullvideoweihao.avi');
+v = VideoReader('fullvideo24fps.avi');
 while hasFrame(v)
     frame = readFrame(v);
     image(frame)
 end
-% Read and Write File with vision.VideoFileWriter
 
-% Create deployable video player and video writer
-VVR = vision.VideoFileReader('fullvideoweihao.avi','VideoOutputDataType', 'uint8' )
+%% Read and Write File with vision.VideoFileWriter
+%Create deployable video player and video writer
+VVR = vision.VideoFileReader('fullvideo24fps.avi','VideoOutputDataType', 'uint8' )
 % depVideoPlayer = vision.DeployableVideoPlayer;
-VVW = vision.VideoFileWriter('AccidentalFile.avi','FrameRate', 55.0001);
-% Write the first 50 frames
-for i = 1:10
+VVW = vision.VideoFileWriter('7minVideo.avi','FrameRate', 24);
+% Write the first i frames
+for i = 1:10080
     videoFrame = VVR();
     VVW(videoFrame);
 end
